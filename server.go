5d7eea9d5f030740a8aaff967bd9ea5e420b83d5
Katherine Threlkeld <kathrelkeld@gmail.com>
2014-10-04 12:05:07 -0700

    Random tile generation

--- server.go
+++ server.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
 )
 
+func newTiles() []string {
+	var tiles []string
+	for k, v := range freqMap {
+		for j := 0; j < v; j++ {
+			tiles = append(tiles, k)
+		}
+	}
+	for i := range tiles {
+		j := rand.Intn(i + 1)
+		tiles[i], tiles[j] = tiles[j], tiles[i]
+	}
+	return tiles
+}
+
 func main() {
+	tiles := newTiles()
+	log.Println(tiles)
+
 	const addr = "localhost:8080"
 	fileserver := http.FileServer(http.Dir("public"))
 	redirect := http.RedirectHandler("public/scrabble.html", http.StatusFound)
@@ -15,4 +33,33 @@ func main() {
 
 	log.Println("Now listening on", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+var freqMap = map[string]int{
+	"A": 13,
+	"B": 3,
+	"C": 3,
+	"D": 6,
+	"E": 18,
+	"F": 3,
+	"G": 4,
+	"H": 3,
+	"I": 12,
+	"J": 2,
+	"K": 2,
+	"L": 5,
+	"M": 3,
+	"N": 8,
+	"O": 11,
+	"P": 3,
+	"Q": 2,
+	"R": 9,
+	"S": 6,
+	"T": 9,
+	"U": 6,
+	"V": 3,
+	"W": 3,
+	"X": 2,
+	"Y": 3,
+	"Z": 2,
 }
